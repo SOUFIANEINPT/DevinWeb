3029a3b386ef5036080b570d83a68f3c4f5cf0e6
soufiane <soufiane.elmoudden.15@gmail.com>
2018-05-16 11:58:08 +0000

    create formlair in front-end with 4 input field  and also flush message for the errors message and loading spinner

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  messgaeError={email:[],name:[],password:[]};
+  constructor(private spinnerService: Ng4LoadingSpinnerService) { }
+  ngOnInit() {
+  }
+  onSignup(form: NgForm) {
+    this.spinnerService.show();
+    const email = form.value.email;
+    const password = form.value.password;
+    const username= form.value.username;
+    
+  }
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 import { AppComponent } from './app.component';
-
-
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FlashMessagesModule.forRoot(),
+    Ng4LoadingSpinnerModule.forRoot() 
   ],
   providers: [],
   bootstrap: [AppComponent]
